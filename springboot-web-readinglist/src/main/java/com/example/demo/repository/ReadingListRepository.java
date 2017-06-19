178dfea521ae3f154d2e1bfa372a68cbac223347
Lino68 <yinyiniao@foxmail.com>
2017-06-19 23:13:49 +0800

    　

---
+++ springboot-web-readinglist/src/main/java/com/example/demo/repository/ReadingListRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.domain.Book;
+
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+	List<Book> findByReader(String reader);
+}
